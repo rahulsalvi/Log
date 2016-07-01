570b21926fd9e52ca20c965449dc753b2bbb2f2f
Rahul Salvi <rsalvi006@me.com>
2016-06-30 18:03:08 -0700

    fixes

--- src/Log.h
+++ src/Log.h
@@ -11,7 +11,11 @@
 #include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
 #include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/utility/empty_deleter.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 namespace logging = boost::log;
 
@@ -25,7 +29,9 @@ enum severityLevel {
     debug
 };
 
-std::ostream& operator<<(std::ostream& stream, severityLevel level)
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severityLevel);
+
+inline std::ostream& operator<<(std::ostream& stream, severityLevel level)
 {
     static const char* strings[] =
     {
@@ -45,9 +51,9 @@ std::ostream& operator<<(std::ostream& stream, severityLevel level)
     return stream;
 }
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severityLevel);
+inline void initializeLog(std::string filename = "") {
+    logging::add_common_attributes();
 
-void initializeLog(std::string filename = "") {
     typedef logging::sinks::synchronous_sink<logging::sinks::text_ostream_backend> text_sink;
     boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
 
@@ -60,15 +66,15 @@ void initializeLog(std::string filename = "") {
     }
 
     logging::formatter format = logging::expressions::stream <<
-        "[" << logging::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S") << "]" <<
-        "[" << severity << "]" <<
+        "[" << logging::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%m-%d-%Y %H:%M:%S") << "]" <<
+        "[" << severity << "] " <<
         logging::expressions::smessage;
     sink->set_formatter(format);
 
     logging::core::get()->add_sink(sink);
 }
 
-void setLogFilter(severityLevel level) {
+inline void setLogFilter(severityLevel level) {
     logging::core::get()->set_filter(severity >= level);
 }
 
